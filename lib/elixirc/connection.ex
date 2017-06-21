f1cd0da201ad56fbfb7791473528c455edaf6e47
HENRY <henry@rhodochrosite.xyz>
2017-06-20 22:27:35 -0600

    Handle socket closes?

--- lib/elixirc/connection.ex
+++ lib/elixirc/connection.ex
@@ -1,13 +1,16 @@
 defmodule Elixirc.Connection do
   def sock_read(socket, buffer) do
-    {:ok, data} = Elixirc.Connection.Connector.recv(socket, 0)
-    buffer = buffer <> data
-    [current | bfs] = String.split(buffer, "\n")
-    buffer = Enum.join(bfs)
-    if current do
-      cmd = Elixirc.Events.parse(current)
-      GenStage.cast(Elixirc.EventManager, {:command, cmd})
+    case Elixirc.Connection.Connector.recv(socket, 0) do
+      {:ok, data} ->
+        buffer = buffer <> data
+        [current | bfs] = String.split(buffer, "\n")
+        buffer = Enum.join(bfs)
+        if current do
+          cmd = Elixirc.Events.parse(current)
+          GenStage.cast(Elixirc.EventManager, {:command, cmd})
+        end
+        sock_read(socket, buffer)
+      {:error, err} -> Elixirc.Connection.Connector.close(socket)
     end
-    sock_read(socket, buffer)
   end
 end

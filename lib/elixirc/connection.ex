497e977e6242951b831048e2a40ec882026e0766
HENRY <henry@rhodochrosite.xyz>
2017-06-22 00:08:25 -0600

    Added logging

--- lib/elixirc/connection.ex
+++ lib/elixirc/connection.ex
@@ -1,9 +1,13 @@
 defmodule Elixirc.Connection do
+  require Logger
+
   def sock_read(socket, buffer) do
     case Elixirc.Connection.Connector.recv(socket, 0) do
       {:ok, data} ->
         buffer = buffer <> data
         [current | bfs] = String.split(buffer, "\n")
+        current
+        |> Logger.info
         buffer = Enum.join(bfs)
         if current do
           cmd = Elixirc.Events.parse(current)
